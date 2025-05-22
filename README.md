36538609193542f710577795f3e05198117bcf38
Alex Ghandhi <alexpghandhi@gmail.com>
2025-05-22 07:02:37 -0600

    update resources and add PPU register r/w logic

--- README.md
+++ README.md
@@ -7,4 +7,6 @@ This repository contains a SystemVerilog implementation of the GameBoy Pixel Pro
 ## Resources
 | Name | Description |
 | ---  | ---         |
-[Pan Docs](https://gbdev.io/pandocs/) | PPU Operation Documentation
+[Pan Docs](https://gbdev.io/pandocs/)                                       | PPU Operation Documentation
+[The Ultimate GameBoy Talk](https://www.youtube.com/watch?v=HyzD8pNlpwI)    | Visual Presentation on PPU Function
+[BameGoy Presentation](https://www.cs.columbia.edu/~sedwards/classes/2024/4840-spring/reports/BameGoy-presentation.pdf) | Notes from a University Project Implementing a GameBoy PPU
--- src/gb_ppu.sv
+++ src/gb_ppu.sv
@@ -3,7 +3,8 @@ import gb_oam_pkg::*;
 /* Top Level Module for GameBoy PPU
 
 Inputs:
-    clk             - System Clock (T Clock, ~4MHz)
+    clk_t           - System Clock  (T Clock, ~4MHz)
+    clk_m           - Machine Clock (M Clock, ~1MHz)
     reset           - System Reset
     addr            - 16-Bit Address Bus
     data_i          - 8-Bit Input Data
@@ -15,7 +16,8 @@ Outputs:
     dma_start_addr  - Start Address for DMA Transfer
 */
 module gb_ppu (
-    input  logic        clk,
+    input  logic        clk_t,
+    input  logic        clk_m,
     input  logic        reset,
     input  logic [15:0] addr,
     input  logic [ 7:0] data_i,
@@ -27,6 +29,12 @@ module gb_ppu (
 
     // Store PPU state information
     ppu_mode_state_t ppu_mode;
+
+    // For Interrupts, store whether PPU is in certain state
+    logic in_mode_0, in_mode_1, in_mode_2;
+    assign in_mode_0 = (ppu_mode == HBLANK) ? 1'b1 : 1'b0;
+    assign in_mode_1 = (ppu_mode == VBLANK) ? 1'b1 : 1'b0;
+    assign in_mode_2 = (ppu_mode == OAM_SCAN) ? 1'b1 : 1'b0;
 
     /* DMG PPU CONTROL REGISTERS (0xFF40-0xFF4B)
         0xFF40 - LCDC (LCD Control Register)
@@ -68,6 +76,14 @@ module gb_ppu (
         STAT.lyc_ly_compare  = (reg_LY == reg_LYC) ? 1'b1 : 1'b0;
         STAT.ppu_mode        = ppu_mode;
     end
+    assign reg_STAT[2] = STAT.lyc_ly_compare;
+    assign reg_STAT[1:0] = STAT.ppu_mode;
+
+    // Handle the STAT Interrupt Request
+    assign irq_stat = (STAT.lyc_ly_compare & STAT.lyc_irq_cond) |
+                      (in_mode_0 & STAT.mode_0_irq_cond) |
+                      (in_mode_1 & STAT.mode_1_irq_cond) |
+                      (in_mode_2 & STAT.mode_2_irq_cond);
 
     // Store Palette Registers
     palette_register_t BGP, OBP0, OBP1;
@@ -86,9 +102,64 @@ module gb_ppu (
         OBP1.id_3 = pixel_color_t'(reg_OBP1[7:6]);
     end
 
-
-
-    // When reg_DMA is written to, raise the dma start signal for 4 T-cycles
+    // Specify DMA Start Address for Transfer
     assign dma_start_addr = {reg_DMA, 8'h00};
+
+    // PPU Register Reads
+    always_comb begin : PPUregRead
+        case (addr) inside
+            16'hFF40: data_o = reg_LCDC;
+            16'hFF41: data_o = reg_STAT;
+            16'hFF42: data_o = reg_SCY;
+            16'hFF43: data_o = reg_SCX;
+            16'hFF44: data_o = reg_LY;
+            16'hFF45: data_o = reg_LYC;
+            16'hFF46: data_o = reg_DMA;
+            16'hFF47: data_o = reg_BGP;
+            16'hFF48: data_o = reg_OBP0;
+            16'hFF49: data_o = reg_OBP1;
+            16'hFF4A: data_o = reg_WY;
+            16'hFF4B: data_o = reg_WX;
+            default:  data_o = 8'hxx;
+        endcase
+    end : PPUregRead
+
+    // PPU Register Writes and DMA Start Signal
+    always_ff @(posedge clk_m, reset)
+        if (reset) begin
+            dma_start <= 1'b0;
+            reg_LCDC  <= 8'h00;
+            reg_STAT  <= 8'h00;
+            reg_SCY   <= 8'h00;
+            reg_SCX   <= 8'h00;
+            reg_LY    <= 8'h00;
+            reg_LYC   <= 8'h00;
+            reg_DMA   <= 8'h00;
+            reg_BGP   <= 8'h00;
+            reg_OBP0  <= 8'h00;
+            reg_OBP1  <= 8'h00;
+            reg_WY    <= 8'h00;
+            reg_WX    <= 8'h00;
+        end else if (wren_cpu) begin
+            case (addr) inside
+                16'hFF40: reg_LCDC <= data_i;
+                16'hFF41: reg_STAT[7:3] <= data_i[7:3];
+                16'hFF42: reg_SCY <= data_i;
+                16'hFF43: reg_SCX <= data_i;
+                16'hFF45: reg_LYC <= data_i;
+                16'hFF46: begin
+                    reg_DMA   <= data_i;
+                    dma_start <= 1'b1;
+                end
+                16'hFF47: reg_BGP <= data_i;
+                16'hFF48: reg_OBP0 <= data_i;
+                16'hFF49: reg_OBP1 <= data_i;
+                16'hFF4A: reg_WY <= data_i;
+                16'hFF4B: reg_WX <= data_i;
+            endcase
+        end else if (dma_start && ~(wren && (addr == 16'hFF46))) begin
+            dma_start <= 1'b0;
+        end
+
 
 endmodule : gb_ppu
