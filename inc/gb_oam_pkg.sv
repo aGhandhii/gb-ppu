474f742a510ac1fb758ecc6f031b9fd1f45f9c42
Alex Ghandhi <alexpghandhi@gmail.com>
2026-03-18 09:22:58 -0600

    add helper function for OAM_SCAN stage

--- inc/gb_oam_pkg.sv
+++ inc/gb_oam_pkg.sv
@@ -18,4 +18,26 @@ package gb_oam_pkg;
         logic     isValid;
     } obj_buffer_t;
 
+    // Detect if an object lands on a scanline
+    function automatic logic objectOnScanline(logic [7:0] obj_y_pos, logic [7:0] reg_LY, logic obj_size);
+        // sprite y-pos value is offset by 16 from the viewport
+        // A y-val of 0 is offscreen,
+        // but a y-val of 1 would be onscreen if the sprite is in 8x16 mode,
+        // and a y-val of 9 is onscreen in 8x8 or 8x16 mode
+
+        if (obj_size) begin
+            // 8x16 Mode
+            if ((obj_y_pos == 8'd0) || (obj_y_pos >= 8'd160)) return 1'b0;
+            else if ( ({obj_y_pos[7:3], 3'b000} == {reg_LY[7:3], 3'b000}) || ({(obj_y_pos[7:3] + 5'd1), 3'b000} == {reg_LY[7:3], 3'b000}) )
+                return 1'b1;
+            else return 1'b0;
+        end else begin
+            // 8x8 Mode
+            if ((obj_y_pos <= 8'd8) || (obj_y_pos >= 8'd160)) return 1'b0;
+            if ({obj_y_pos[7:3], 3'b000} == {reg_LY[7:3], 3'b000}) return 1'b1;
+            else return 1'b0;
+        end
+
+    endfunction : objectOnScanline
+
 endpackage : gb_oam_pkg
